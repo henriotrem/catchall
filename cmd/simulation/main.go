31afcd064da21cbcb71ad0ead50eaff34608c5b8
Remi Henriot <henriot.rem@gmail.com>
2021-10-03 22:08:27 +0200

    Fix removing existing files

--- cmd/simulation/main.go
+++ cmd/simulation/main.go
@@ -2,53 +2,79 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"runtime"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
 
-	nClusters := 3
-	clusterSize := 3
-	var clusters string
+	var tmp, cluster, clusters string
 	var args []string
+	var nClusters = 3
+	var clusterSize = 3
+
+	if len(os.Args) == 3 {
+		nClusters, _ = strconv.Atoi(os.Args[1])
+		clusterSize, _ = strconv.Atoi(os.Args[2])
+	}
+
+	fmt.Println("\n# Starting 'CatchAll - Simulation' application")
 
 	// Delete existing logs
-	exec.Command("rmdir", "clusters").Run()
+	os.RemoveAll("clusters")
+	fmt.Println("* Previous logs deleted")
 
 	// Create Worker Clusters
 	for clIdx := 1; clIdx <= nClusters; clIdx++ {
 
 		// Start Worker Cluster Servers
+		fmt.Println("* Application starts the fault tolerant worker cluster :", clIdx)
 		for i := 0; i < clusterSize; i++ {
-			clusters, args = workerClusterArgs(clusters, i, clIdx)
-			go exec.Command("worker.exe", args...).Run()
+			tmp, args = workerClusterArgs(i, clIdx)
+			cluster += tmp
+			go exec.Command(createName("worker"), args...).Run()
+			fmt.Println("* Worker node", i, "started")
 
 			if i == 0 {
 				time.Sleep(5 * time.Second)
 			}
 		}
-
 		// Start Worker Web Servers
-		go exec.Command("worker-api.exe", createArgs("127.0.0.1:1100%d 2200%d ", clIdx, clIdx)...).Run()
+		go exec.Command(createName("worker-api"), createArgs("127.0.0.1:1100%d 2200%d ", clIdx, clIdx)...).Run()
+		fmt.Println("* Web worker server", clIdx, "started connected to", cluster)
+
+		clusters, cluster = clusters+cluster, ""
 	}
 
 	// Start Master Aggregator
-	fmt.Println(clusters)
-	go exec.Command("master.exe", createArgs("127.0.0.1:27017"+clusters)...).Run()
+	go exec.Command(createName("master"), createArgs("127.0.0.1:27017"+clusters)...).Run()
+	fmt.Println("* Aggregator master service started")
 
 	// Start Master Aggregator
-	go exec.Command("master-api.exe", createArgs("127.0.0.1:27017 8085")...).Run()
+	go exec.Command(createName("master-api"), createArgs("127.0.0.1:27017 8085")...).Run()
+	fmt.Println("* Web master server started")
+	fmt.Println("\n# READY TO RECEIVE EVENTS")
 
 	time.Sleep(1 * time.Hour)
+}
+
+func createName(name string) string {
+	if runtime.GOOS == "windows" {
+		return name + ".exe"
+	} else {
+		return "./" + name
+	}
 }
 
 func createArgs(str string, numbers ...interface{}) []string {
 	return strings.Split(fmt.Sprintf(str, numbers...), " ")
 }
 
-func workerClusterArgs(clusters string, node int, clIdx int) (string, []string) {
+func workerClusterArgs(node int, clIdx int) (string, []string) {
 	var args []string
 	port := fmt.Sprintf(":110%d%d", node, clIdx)
 	separator := ""
@@ -59,6 +85,6 @@ func workerClusterArgs(clusters string, node int, clIdx int) (string, []string) 
 		args = createArgs("-d clusters/%d -n %d -a "+port+" -j 127.0.0.1:1100%d", clIdx, node+1, clIdx)
 		separator = ","
 	}
-	clusters += separator + "127.0.0.1" + port
-	return clusters, args
+	cluster := separator + "127.0.0.1" + port
+	return cluster, args
 }
