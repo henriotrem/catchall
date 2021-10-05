5af0628229d11cc52bf9bcad0da50c61617f7af3
Remi Henriot <henriot.rem@gmail.com>
2021-10-05 16:35:09 +0200

    Improving the benchmark tool and making it only usable with the HTTP protocol

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -6,99 +6,67 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
-	"github.com/garyburd/redigo/redis"
 	"github.com/mailgun/catchall"
-)
-
-const (
-	MODE_HTTP  = "http"
-	MODE_REDIS = "redis"
 )
 
 func main() {
 
 	if len(os.Args) < 4 {
-		fmt.Fprintf(os.Stderr, "Specify the type of benchkmark you want to perform [redis,http] [nClusters] [nThreads]")
-		os.Exit(1)
-	}
-	if os.Args[1] != MODE_HTTP && os.Args[1] != MODE_REDIS {
-		fmt.Fprintf(os.Stderr, "Use the following modes [http,redis]")
+		fmt.Fprintf(os.Stderr, "Specify the type of benchkmark you want to perform [clusters] [nThreads] [calls]")
 		os.Exit(1)
 	}
 	fmt.Println("\n# Starting 'CatchAll - Benchmark' application")
-	fmt.Println("* Application starting the benchmark using", os.Args[1])
-	fmt.Println("* Clusters tested", os.Args[2])
+	fmt.Println("* Web servers called : ", os.Args[1])
 
-	nClusters, _ := strconv.Atoi(os.Args[2])
-	nThreads, _ := strconv.Atoi(os.Args[3])
-	wg := &sync.WaitGroup{}
+	endpoints := strings.Split(os.Args[1], ",")
+
+	nThreads, _ := strconv.Atoi(os.Args[2])
+	tCalls, _ := strconv.Atoi(os.Args[3])
+
 	bus := catchall.SpawnEventPool()
 	defer bus.Close()
 
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = nThreads
+
+	wg := &sync.WaitGroup{}
 	for i := 0; i < nThreads; i++ {
-		wg.Add(1)
-		if os.Args[1] == MODE_HTTP {
-			go mailgunHttp(i, nClusters, nThreads, bus, wg)
-		} else if os.Args[1] == MODE_REDIS {
-			go mailgunRedis(i, nClusters, nThreads, bus, wg)
+		nCalls := tCalls / nThreads
+		if i < tCalls%nThreads {
+			nCalls++
 		}
+		wg.Add(1)
+		go httpGun(i, nCalls, nThreads, endpoints, bus, wg)
 	}
 
 	wg.Wait()
+	fmt.Printf("* Completed: %d \n", tCalls)
 }
 
-func mailgunHttp(idx int, nClusters int, nThreads int, bus catchall.EventPool, wg *sync.WaitGroup) {
-
-	client := &http.Client{}
+// Reusing the connection
+func httpGun(idx int, calls int, nThreads int, endpoints []string, bus catchall.EventPool, wg *sync.WaitGroup) {
 
 	init := time.Now().UnixNano()
+	nEndpoints := len(endpoints)
 
-	for i := 1; i <= 500_000; i++ {
+	for i := 1; i <= calls; i++ {
 		event := bus.GetEvent()
 
-		url := "http://127.0.0.1:2200" + strconv.Itoa(i%nClusters+1) + "/events/" + event.Domain + "/" + event.Type
+		url := endpoints[i%nEndpoints] + "/events/" + event.Domain + "/" + event.Type
 		req, err := http.NewRequest(http.MethodPut, url, nil)
 		if err != nil {
-			log.Fatalf("Wrong URL: %v\n", err)
+			log.Fatal(err)
 		}
 
-		_, err = client.Do(req)
+		_, err = http.DefaultClient.Do(req)
 		if err != nil {
-			log.Fatalf("Could not INCR: %v\n", err)
+			log.Fatal(err)
 		}
 
-		if idx%nThreads == 0 && i%1_000 == 0 {
-			printStatistics(nThreads, init, i)
-		}
-
-		bus.RecycleEvent(event)
-	}
-
-	wg.Done()
-}
-
-func mailgunRedis(idx int, nClusters int, nThreads int, bus catchall.EventPool, wg *sync.WaitGroup) {
-
-	c, err := redis.Dial("tcp", ":11001")
-	if err != nil {
-		log.Fatalf("Could not connect: %v\n", err)
-	}
-	defer c.Close()
-
-	init := time.Now().UnixNano()
-
-	for i := 1; i <= 500_000; i++ {
-		event := bus.GetEvent()
-
-		_, err := c.Do("INCR", event.Domain, event.Type == catchall.TypeDelivered, event.Type == catchall.TypeBounced)
-		if err != nil {
-			log.Fatalf("could not INCR: %v\n", err)
-		}
-
-		if idx%nThreads == 0 && i%1_000 == 0 {
+		if idx == 0 && i%1_000 == 0 {
 			printStatistics(nThreads, init, i)
 		}
 
@@ -109,9 +77,7 @@ func mailgunRedis(idx int, nClusters int, nThreads int, bus catchall.EventPool, 
 }
 
 func printStatistics(nThreads int, init int64, i int) {
-	now := time.Now().UnixNano()
-	duration := (now - init) / 1_000
 	inserted := int64(i * nThreads)
-	throughput := inserted * 1_000_000 / duration
+	throughput := inserted * 1_000_000 / ((time.Now().UnixNano() - init) / 1_000)
 	fmt.Printf("* Inserted: %d | Speed: %d/sec \n", inserted, throughput)
 }
